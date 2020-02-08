9311063c6f4db5eb621acbaebfbabf7a42e7da89
Gutnar Leede <gutnar@gmail.com>
2020-02-08 11:34:10 +0200

    Add broadcasting updateProduct messages

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 const products = {
-    P1: { id: 'P1', type: 'dishwashing', name: 'Fairy', price: 2.3 },
-    P2: { id: 'P2', type: 'soap', name: 'Decent Liquid Soap', price: 3.5 },
-    P3: { id: 'P3', type: 'shampoo', name: 'Dove Shampoo', price: 4.2 }
+    P1: { id: 'P1', type: 'dishwashing', name: 'Fairy', price: 2.3, capacity: 20, available: 18, dispensing: false },
+    P2: { id: 'P2', type: 'soap', name: 'Decent Liquid Soap', price: 3.5, capacity: 20, available: 16.7, dispensing: false },
+    P3: { id: 'P3', type: 'shampoo', name: 'Dove Shampoo', price: 4.2, capacity: 20, available: 4.3, dispensing: false }
 };
 
 const machines = {
@@ -22,9 +22,29 @@ wss.on('connection', ws => {
     }
 
     ws.on('message', json => {
-        const data = JSON.parse(json);
+        try {
+            const { event, data } = JSON.parse(json);
 
-        switch (data.event) {
+            switch (event) {
+                case 'updateProduct':
+                    if (!(data.id in products)) {
+                        throw new Error(`Product '${data.id}' not known`);
+                    }
+
+                    products[data.id] = {
+                        ...products[data.id],
+                        ...data
+                    };
+
+                    for (let client of wss.clients) {
+                        if (client.readyState === WebSocket.OPEN) {
+                            client.send(json);
+                        }
+                    }
+                    break;
+            }
+        } catch (err) {
+            console.log(err);
         }
     });
 });
